be86fe4d3dda038f2ff755526cc0a35ee77c0df6
Clarefollett <chlaw101@gmail.com>
2017-04-09 10:40:35 -0700

    added selection bar so you're able to add new todos

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import TodoList from './TodoList';
+import SelectionBar from './SelectionBar';
 
 export default function App(props) {
     return (
        <div>
-           <h1>Todo</h1>
+           <h1>Todo List</h1>
            < TodoList />
+           < SelectionBar />
        </div>
     );
 }
---
+++ src/components/SelectionBar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddTodo from './AddTodo';
+
+export default function SelectionBar(props) {
+    return (
+        <div>
+            <AddTodo />
+        </div>
+    );
+};
