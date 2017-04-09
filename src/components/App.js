12f188c2a13ab3ebd8e19a25c337a2968ead1e23
Clarefollett <chlaw101@gmail.com>
2017-04-09 10:02:33 -0700

    added App.js and index, the app is running w/o errors and is displaying 'todo' so I think thats progress!

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import TodoList from './TodoList';
+
+export default function App(props) {
+    return (
+       <div>
+           <h1>Todo</h1>
+           < TodoList />
+       </div>
+    );
+}
